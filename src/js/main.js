055a4532f7d42fa2485a3e9f23e0e4856ee5a96e
nazmieievilya <ilyanazmeev111@gmail.com>
2023-01-12 17:55:30 +0300

    blog section

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,59 @@
 // Мобильное меню бургер
 function burgerMenu() {
-    const burger = document.querySelector('.burger')
-    const menu = document.querySelector('.menu')
-    const body = document.querySelector('body')
-    burger.addEventListener('click', () => {
-        if (!menu.classList.contains('active')) {
-            menu.classList.add('active')
-            burger.classList.add('active')
-            body.classList.add('locked')
-        } else {
-            menu.classList.remove('active')
-            burger.classList.remove('active')
-            body.classList.remove('locked')
-        }
-    })
-    // Вот тут мы ставим брейкпоинт навбара
-    window.addEventListener('resize', () => {
-        if (window.innerWidth > 991.98) {
-            menu.classList.remove('active')
-            burger.classList.remove('active')
-            body.classList.remove('locked')
-        }
-    })
+  const burger = document.querySelector(".burger");
+  const menu = document.querySelector(".menu");
+  const body = document.querySelector("body");
+  burger.addEventListener("click", () => {
+    if (!menu.classList.contains("active")) {
+      menu.classList.add("active");
+      burger.classList.add("active");
+      body.classList.add("locked");
+    } else {
+      menu.classList.remove("active");
+      burger.classList.remove("active");
+      body.classList.remove("locked");
+    }
+  });
+  // Вот тут мы ставим брейкпоинт навбара
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 991.98) {
+      menu.classList.remove("active");
+      burger.classList.remove("active");
+      body.classList.remove("locked");
+    }
+  });
 }
-burgerMenu()
+burgerMenu();
+
+// Swiper Slider
+const swiper = new Swiper(".swiper_testimonials", {
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider_btn_next",
+    prevEl: ".slider_btn_prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+
+  //   // Responsive breakpoints
+  //   breakpoints: {
+  //   // when window width is >= 320px
+  //   320: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 20
+  //   },
+  //   // when window width is >= 480px
+  //   480: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 30
+  //   },
+  // }
+});
